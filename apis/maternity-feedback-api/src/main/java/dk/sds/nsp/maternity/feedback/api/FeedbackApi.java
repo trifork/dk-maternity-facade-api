94136c2b4744a9dc9ceecf41a51986b6327eb05b
Jeppe Gravgaard <jgg@trifork.com>
2020-10-20 09:27:31 +0200

    added SlaLogger part 2

--- apis/maternity-feedback-api/src/main/java/dk/sds/nsp/maternity/feedback/api/FeedbackApi.java
+++ apis/maternity-feedback-api/src/main/java/dk/sds/nsp/maternity/feedback/api/FeedbackApi.java
@@ -1,15 +1,20 @@
 package dk.sds.nsp.maternity.feedback.api;
 
+import dk.sds.nsp.maternity.cfg.SlaLogger;
 import dk.sds.nsp.maternity.facade.common.jaxrs.RequestContext;
 import dk.sds.nsp.maternity.feedback.model.Feedback;
 import dk.sds.nsp.maternity.feedback.model.FeedbackResponse;
 import dk.sds.nsp.maternity.feedback.service.FeedbackService;
 import dk.sds.nsp.maternity.feedback.spring.DependencyResolver;
+import org.apache.http.HttpStatus;
+import org.apache.log4j.Logger;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.net.URI;
@@ -19,21 +24,36 @@ import java.net.URI;
 public class FeedbackApi {
 
     private final FeedbackService service;
+    private final SlaLogger slalogger;
+    private static final Logger LOGGER = Logger.getLogger(FeedbackApi.class);
 
     public FeedbackApi() {
         service = DependencyResolver.getFeedbackService();
+        slalogger = new SlaLogger("FeedbackAPI_PostFeedback");
     }
+
+    @Context private HttpServletRequest httpServletRequest;
 
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
-    public Response createDataCard(
+    public Response createFeedback(
             final Feedback feedback) {
 
-        final FeedbackResponse response = service.create(feedback);
-        return Response
-                .created(getLocation(response))
-                .entity(response)
-                .build();
+        Long startTimeNano = System.nanoTime();
+        Long startTimeMillis = System.currentTimeMillis();
+        try{
+            final FeedbackResponse response = service.create(feedback);
+            slalogger.addSlaLog(httpServletRequest, startTimeNano, startTimeMillis, null);
+            return Response
+                    .created(getLocation(response))
+                    .entity(response)
+                    .build();
+
+        } catch (Exception e) {
+            LOGGER.error("Could not post feedback", e);
+            slalogger.addSlaLog(httpServletRequest, startTimeNano, startTimeMillis, e.getMessage());
+            return Response.status(HttpStatus.SC_BAD_GATEWAY).build();
+        }
     }
 
     private static URI getLocation(final FeedbackResponse created) {
---
+++ cfg/src/main/java/dk/sds/nsp/maternity/cfg/SlaLogger.java
@@ -0,0 +1,99 @@
+/*
+ * The MIT License
+ *
+ * Original work sponsored and donated by The Danish Health Data Authority (http://www.sundhedsdatastyrelsen.dk)
+ *
+ * Copyright (C) 2017 The Danish Health Data Authority (http://www.sundhedsdatastyrelsen.dk)
+ *
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is furnished to do
+ * so, subject to the following conditions:
+ *
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package dk.sds.nsp.maternity.cfg;
+
+import dk.sdsd.nsp.slalog.api.SLALogConfig;
+import dk.sdsd.nsp.slalog.api.SLALogItem;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * This SlaLogger is used for SLA logging http calls as is a requirement by NSP.
+ */
+public class SlaLogger {
+
+    private static final Logger logger = LogManager.getLogger(SlaLogger.class);
+
+    private static final Integer lock = 1;
+    public static final String SHORT_APP_NAME = "maternity-web-facade";
+    private static SLALogConfig slaLogInstance;
+
+    private String name;
+
+    public SlaLogger(String name) {
+        this.name = name;
+        synchronized (lock) {
+            if (slaLogInstance == null) {
+                slaLogInstance = new SLALogConfig(SHORT_APP_NAME, SHORT_APP_NAME);
+            }
+        }
+    }
+
+    /**
+     * creates a SLAlog Item
+     * @param httpServletRequest The servlet request context
+     * @param startTimeNano Start time in nano seconds
+     * @param startTimeMillis Start time in milliseconds seconds
+     * @param errorMessage optional error message
+     * @return SlaLogItem
+     */
+    protected SLALogItem createSlaLogItem(HttpServletRequest httpServletRequest, Long startTimeNano, Long startTimeMillis, String errorMessage){
+        SLALogItem logItem = slaLogInstance.getSLALogger().createLogItem(SHORT_APP_NAME + "." + name, SHORT_APP_NAME + "." + name);
+
+        // SLA headers that is trackable in REST interfaces
+        logItem.setStartTime(startTimeNano, startTimeMillis);
+        logItem.setClientIP(httpServletRequest);
+        logItem.setSourceEndpoint(httpServletRequest.getServletPath());
+        logItem.setSourceOperation(httpServletRequest.getMethod());
+        logItem.setEndTime(System.nanoTime(), System.currentTimeMillis());
+
+        if (errorMessage == null) {
+            logItem.setCallResultOk();
+        } else {
+            logItem.setCallResultError(errorMessage);
+        }
+        return logItem;
+    }
+    /**
+     * create and stores a SLA log
+     * @param httpServletRequest The servlet request context
+     * @param startTimeNano Start time in nano seconds
+     * @param startTimeMillis Start time in milliseconds seconds
+     * @param errorMessage optional error message
+     */
+    public void addSlaLog(HttpServletRequest httpServletRequest, Long startTimeNano, Long startTimeMillis,String errorMessage) {
+        try {
+            createSlaLogItem(httpServletRequest, startTimeNano, startTimeMillis, errorMessage).store();
+        } catch (Exception e) {
+            logger.error("Could not create SLA LogItem", e);
+        }
+    }
+}
