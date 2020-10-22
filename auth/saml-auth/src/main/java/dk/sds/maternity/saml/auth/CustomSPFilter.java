0972d23debcf8f5d5052013c0624dcff8ba94cd2
Jeppe Gravgaard <jgg@trifork.com>
2020-10-22 10:57:01 +0200

    Skip SpFilter Paths

--- auth/saml-auth/src/main/java/dk/sds/maternity/saml/auth/CustomSPFilter.java
+++ auth/saml-auth/src/main/java/dk/sds/maternity/saml/auth/CustomSPFilter.java
@@ -6,12 +6,26 @@ import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 
 public class CustomSPFilter extends SPFilter {
+    private final List<String> skipSPFilterPaths = Arrays.asList(
+        "/youAreLoggedOut",
+        "/isAlive"
+            );
 
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest httpServletRequest = ((HttpServletRequest) request);
+
+        // bypass the SPFilter
+        if(skipSPFilterPaths.stream().anyMatch(path -> httpServletRequest.getRequestURI().startsWith(path))){
+            chain.doFilter(request, response);
+            return;
+        }
         super.doFilter(request, response, chain);
     }
 }
