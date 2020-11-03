74a216b0c96c63bd71e438e744591669d9ee6eb6
Jeppe Gravgaard <jgg@trifork.com>
2020-11-03 12:54:58 +0100

    renames session to usercontext

--- auth/saml-auth/src/main/java/dk/sds/maternity/saml/auth/restresources/UserContext.java
+++ auth/saml-auth/src/main/java/dk/sds/maternity/saml/auth/restresources/UserContext.java
@@ -2,7 +2,6 @@ package dk.sds.maternity.saml.auth.restresources;
 
 import dk.itst.oiosaml.sp.UserAssertion;
 import dk.itst.oiosaml.sp.UserAssertionHolder;
-import dk.sds.maternity.saml.auth.model.UserContext;
 import org.apache.log4j.Logger;
 
 import javax.servlet.http.HttpServletRequest;
@@ -13,20 +12,20 @@ import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
-@Path("session")
+@Path("usercontext")
 @Produces(MediaType.APPLICATION_JSON)
-public class Session {
-    private static final Logger LOGGER = Logger.getLogger(Session.class);
+public class UserContext {
+    private static final Logger LOGGER = Logger.getLogger(UserContext.class);
     @GET
     public Response getSessionDetails(@Context HttpServletRequest request){
         final UserAssertion userAssertion = UserAssertionHolder.get();
 
         if(userAssertion == null) return Response.ok().build();
 
-        UserContext userContext = new UserContext()
+        dk.sds.maternity.saml.auth.model.UserContext userContext = new dk.sds.maternity.saml.auth.model.UserContext()
             .setCommonName(userAssertion.getCommonName())
             .setOrganizationName(userAssertion.getOrganizationName())
-            .setRole(userAssertion.getTitle());
+            .setEligibleRoles(null);
         LOGGER.debug(userContext);
         return Response.ok().entity(userContext).build();
     }
